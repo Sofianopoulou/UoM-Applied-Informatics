e469400e1c47e05099512e45b85b7972e04074be
Apostolia Maria Sofianopoulou <81187477+Sofianopoulou@users.noreply.github.com>
2023-02-20 16:21:59 +0200

    Add files via upload

---
+++ 5th semester/Web Services and Transactions/2023exams/iis21118/WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="iis21118.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            color: #CC66FF;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <span class="auto-style1"><strong>iis21118</strong></span><br />
+            <br />
+            Coach Names: <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" Width="192px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Κλήση Υπηρεσίας" />
+            <br />
+            <br />
+            Team Name:
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+            <br />
+            <br />
+            Country Flag:<br />
+            <asp:Image ID="Image1" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 5th semester/Web Services and Transactions/2023exams/iis21118/WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace iis21118
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        eu.dataaccess.footballpool.ws.Info fbWS = new
+                       eu.dataaccess.footballpool.ws.Info();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                DropDownList1.Items.Add("-Select-");
+
+                eu.dataaccess.footballpool.ws.tTeamCoachName[] coachNames =
+                    fbWS.AllTeamCoachNames();
+
+                for(int i=0; i<coachNames.Length; i++)
+                {
+                    DropDownList1.Items.Add(coachNames[i].sCoachName);
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string coachName = DropDownList1.Text;
+            eu.dataaccess.footballpool.ws.tTeamCoachName[] coachNames =
+                    fbWS.AllTeamCoachNames();
+
+            eu.dataaccess.footballpool.ws.tPlayerName[] playerNames =
+                fbWS.AllPlayerNames(true);
+
+            string teamName="";
+            for (int i = 0; i < coachNames.Length; i++)
+            {
+                if (coachNames[i].sCoachName == coachName)
+                {
+                    Label1.Text = coachNames[i].sTeamName;
+                    teamName = coachNames[i].sTeamName;
+                }
+            }
+
+            for (int i = 0; i < playerNames.Length; i++)
+            {
+                if (playerNames[i].sCountryName == teamName)
+                    Image1.ImageUrl = playerNames[i].sCountryFlag;
+            }    
+        }
+     }
+}
